94a1102bf619dfcb76876066a52d42dcb48b31df
PirateGuy97 <ryanpmarkley@gmail.com>
2020-07-20 14:35:02 +1000

    NEW: login and logout functions for 1.4 submission

--- web/public/app.js
+++ web/public/app.js
@@ -1,5 +1,7 @@
 $('#navbar').load('navbar.html');
 $('#footer').load('footer.html');
+const users = JSON.parse(localStorage.getItem('users')) || [];
+localStorage.setItem('users', JSON.stringify(users));
 const devices = JSON.parse(localStorage.getItem('devices')) || [];
 localStorage.setItem('devices', JSON.stringify(devices));
 
@@ -23,3 +25,49 @@ $('#send-command').on('click', function() {
     const command = $('#command').val();
     console.log(`command is: ${command}`);
 });
+
+$('#register').on('click', function() {
+    const username = $('#username').val();
+    const password = $('#password').val();
+    const confirm = $('#confirm').val();
+    const exists = users.find((user) => {
+        return user.name === username;
+    });
+    if (typeof exists === "undefined") {
+        $("message").empty();
+        $("message").append("<p>Given username has already been used.</p>");
+    } else {
+        if (password === confirm) {
+            users.push({ username, password });
+            location.href = '/login';
+        } else {
+            $("message").empty();
+            $("message").append("<p>Password and Confirm inputs are different.</p>");
+        }
+    }
+});
+
+$('#login').on('click', function() {
+    const username = $('#username').val();
+    const password = $('#password').val();
+    const exists = users.find((user) => {
+        return user.name === username;
+    });
+    if (typeof exists === "undefined") {
+        $("message").empty();
+        $("message").append("<p>Given username was not found.</p>");
+    } else {
+        if (exists.password === password) {
+            localStorage.setItem('isAuthenticated', true);
+            location.href = '/';
+        } else {
+            $("message").empty();
+            $("message").append("<p>Given password was incorrect.</p>");
+        }
+    }
+});
+
+const logout = () => {
+    localStorage.removeItem('isAuthenticated');
+    location.href = '/login';
+}
