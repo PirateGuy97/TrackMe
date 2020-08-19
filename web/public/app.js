6bba69b70b4deafa86617ee6bf7ddae1681a2b58
PirateGuy97 <ryanpmarkley@gmail.com>
2020-08-20 08:29:25 +1000

    I should have done this before now

--- web/public/app.js
+++ web/public/app.js
@@ -1,24 +1,41 @@
 $('#navbar').load('navbar.html');
 $('#footer').load('footer.html');
+const API_URL = 'http://localhost:5000/api';
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://defaultUser:defaultPassword@cluster0.e0ode.mongodb.net', {useNewUrlParser: true, useUnifiedTopology: true });
 const users = JSON.parse(localStorage.getItem('users')) || [];
 localStorage.setItem('users', JSON.stringify(users));
 const devices = JSON.parse(localStorage.getItem('devices')) || [];
-localStorage.setItem('devices', JSON.stringify(devices));
-
-devices.forEach(function(device) {
-    $('#devices tbody').append(`
+const response = $.get(`${API_URL}/devices`).then(response => {
+    response.forEach(device => {
+        $('#devices tbody').append(`
         <tr>
             <td>${device.user}</td>
             <td>${device.name}</td>
         </tr>`
-    );
+        );
+    });
+}).catch(error => {
+    console.error(`Error: ${error}`);
 });
 
-$('#add-device').on('click', function() {
+
+$('#add-device').on('click', () => {
     const user = $('#user').val();
     const name = $('#name').val();
-    devices.push({ user, name });
-    location.href = '/';
+    const sensorData = [];
+
+    const body = {
+        name,
+        user,
+        sensorData
+    };
+
+    $.post(`${API_URL}/devices`, body).then(response => {
+        location.href = '/';
+    }).catch(error => {
+        console.error(`Error: ${error}`);
+    });
 });
 
 $('#send-command').on('click', function() {
